e9d0a4dda73225b44d6a3a2f86bb8e652eea6745
Franco Herrera <franncoherrera2011@gmail.com>
2025-05-28 21:47:20 -0300

    bugfix: styles pokemons grid fixed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
     <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
       <div className="flex h-full">
         <Sidebar />
-        <div className="w-full ml-[40px] text-slate-900">{children}</div>
+        <div className="w-full md:ml-[340px] ml-[40px] text-slate-900">{children}</div>
       </div>
     </div>
   );
